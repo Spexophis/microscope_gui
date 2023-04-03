a3e904a0b91e5a9700593edce86c8805cedf1d88
Spexophis <billinpro@gmail.com>
2023-04-03 22:38:41 +0200

    Update controller_view.py

--- controllers/controller_view.py
+++ controllers/controller_view.py
@@ -1,16 +1,23 @@
+import numpy as np
+from PyQt5.QtCore import pyqtSlot
+
 class ViewController():
 
     def __init__(self, view):
         self.view = view
 
+    @pyqtSlot(np.ndarray)
     def plot_main(self, data):
         self.view.setImage('Main Camera', data)
-        
+
+    @pyqtSlot(np.ndarray)
     def plot_fft(self, data):
         self.view.setImage('FFT', data)
-    
+
+    @pyqtSlot(np.ndarray)
     def plot_sh(self, data):
         self.view.setImage('ShackHartmann', data)
-    
+
+    @pyqtSlot(np.ndarray)
     def plot_wf(self, data):
         self.view.setImage('Wavefront', data)
