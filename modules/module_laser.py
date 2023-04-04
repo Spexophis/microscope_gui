19ba4451f3c66beb9aa9b9b90bf7c493ccf03e95
Spexophis <billinpro@gmail.com>
2023-04-04 21:18:11 +0200

    modifications

--- modules/module_laser.py
+++ modules/module_laser.py
@@ -1,11 +1,11 @@
 import pycobolt
 
 
-class CoboltLaser():
-    
+class CoboltLaser:
+
     def __init__(self):
         super().__init__()
-        
+
         try:
             self.l405 = pycobolt.CoboltLaser(port='COM13')
             print('405 nm Laser Connected')
@@ -34,8 +34,7 @@ class CoboltLaser():
         except:
             print("488 nm Laser #2 Not Found")
             self.l488_2_handle = False
-            
-        
+
     def __del__(self):
         if self.l405_handle:
             del self.l405
@@ -48,14 +47,14 @@ class CoboltLaser():
 
     def all_off(self):
         if self.l405_handle:
-            self.l405.send_cmd('l0')    
+            self.l405.send_cmd('l0')
         if self.l488_0_handle:
             self.l488_0.send_cmd('l0')
         if self.l488_1_handle:
             self.l488_1.send_cmd('l0')
         if self.l488_2_handle:
             self.l488_2.send_cmd('l0')
-        
+
     def all_on(self):
         if self.l405_handle:
             self.l405.send_cmd('l1')
@@ -65,11 +64,11 @@ class CoboltLaser():
             self.l488_1.send_cmd('l1')
         if self.l488_2_handle:
             self.l488_2.send_cmd('l1')
-    
+
     def laserON_488_0(self):
         if self.l488_0_handle:
             self.l488_0.send_cmd('l1')
-    
+
     def laserON_488_1(self):
         if self.l488_1_handle:
             self.l488_1.send_cmd('l1')
@@ -77,7 +76,7 @@ class CoboltLaser():
     def laserON_488_2(self):
         if self.l488_2_handle:
             self.l488_2.send_cmd('l1')
-                        
+
     def laserON_405(self):
         if self.l405_handle:
             self.l405.send_cmd('l1')
@@ -89,47 +88,47 @@ class CoboltLaser():
     def laserOFF_488_1(self):
         if self.l488_1_handle:
             self.l488_1.send_cmd('l0')
-            
+
     def laserOFF_488_2(self):
         if self.l488_2_handle:
             self.l488_2.send_cmd('l0')
-            
+
     def laserOFF_405(self):
         if self.l405_handle:
             self.l405.send_cmd('l0')
-            
+
     def constant_power_488_0(self, p488_0):
         if self.l488_0_handle:
             self.l488_0.constant_power(p488_0)
-    
+
     def constant_power_488_1(self, p488_1):
         if self.l488_1_handle:
             self.l488_1.constant_power(p488_1)
-            
+
     def constant_power_488_2(self, p488_2):
         if self.l488_2_handle:
             self.l488_2.constant_power(p488_2)
-                       
+
     def constant_power_405(self, p405):
         if self.l405_handle:
             self.l405.constant_power(p405)
-            
+
     def modulation_mode_488_0(self, p488_0):
         if self.l488_0_handle:
             self.l488_0.modulation_mode(p488_0)
             self.l488_0.digital_modulation(enable=1)
-            
+
     def modulation_mode_488_1(self, p488_1):
         if self.l488_1_handle:
             self.l488_1.modulation_mode(p488_1)
             self.l488_1.digital_modulation(enable=1)
-            
+
     def modulation_mode_488_2(self, p488_2):
         if self.l488_2_handle:
             self.l488_2.modulation_mode(p488_2)
             self.l488_2.digital_modulation(enable=1)
-            
+
     def modulation_mode_405(self, p405):
         if self.l405_handle:
             self.l405.modulation_mode(p405)
-            self.l405.digital_modulation(enable=1)          
+            self.l405.digital_modulation(enable=1)
