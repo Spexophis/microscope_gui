3d55a61f042359362a5b31f43ec20958340794df
Spexophis <billinpro@gmail.com>
2023-05-29 10:04:03 +0200

    Update module_main.py

--- modules/module_main.py
+++ modules/module_main.py
@@ -1,6 +1,5 @@
 from modules import module_andorixon
 from modules import module_hamamatsu
-from modules import module_tis
 from modules import module_deformablemirror
 from modules import module_laser
 from modules import module_nidaq
@@ -13,7 +12,6 @@ class MainModule:
     def __init__(self):
         self.ccdcam = module_andorixon.EMCCDCamera()
         self.scmoscam = module_hamamatsu.HamamatsuCameraMR()
-        self.tiscam = module_tis.TISCamera()
         self.dm = module_deformablemirror.DeformableMirror()
         self.laser = module_laser.CoboltLaser()
         self.daq = module_nidaq.NIDAQ()
@@ -23,7 +21,6 @@ class MainModule:
     def close(self):
         self.ccdcam.close()
         self.scmoscam.close()
-        self.tiscam.close()
         self.laser.all_off()
         self.dm.reset_dm()
         self.daq.reset_daq()
