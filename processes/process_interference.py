1fa3ff55508d09c57c2f34f8837cbc4594d13b4e
Spexophis <billinpro@gmail.com>
2023-06-22 23:34:17 +0200

    Update process_interference.py

--- processes/process_interference.py
+++ processes/process_interference.py
@@ -1,5 +1,4 @@
 import numpy as np
-import tifffile as tf
 from numpy.fft import fft2, fftshift
 from skimage.restoration import unwrap_phase
 
@@ -7,7 +6,10 @@ from skimage.restoration import unwrap_phase
 class Interferometer:
 
     def __init__(self):
-        pass
+        self.x = 409
+        self.y = 279
+        self.hsx = 128
+        self.hsy = 128
 
     def reconstruction_surface(self, img, msk):
         imf = np.fft.fftshift(np.fft.fft2(img * msk))
@@ -18,11 +20,17 @@ class Interferometer:
         return phase, phase_unwrapped
 
     def extract_order(self, df):
-        coordinates = self.find_order(df)
-        y, x = coordinates[0]
-        ny, nx = df.shape
+        return fftshift(df[self.y - self.hsy: self.y + self.hsy, self.x - self.hsx:self.x + self.hsx])
 
-        return fftshift(df[y - hsy: y + hsy, x - hsx:x + hsx])
-
-
-
+    @staticmethod
+    def circular_mask(radius, size, circle_centre=(0, 0)):
+        m = np.zeros((size, size))
+        coord = np.arange(0.5, size, 1.0)
+        x, y = np.meshgrid(coord, coord)
+        x -= size / 2.
+        y -= size / 2.
+        x -= circle_centre[0]
+        y -= circle_centre[1]
+        mask = x ** 2 + y ** 2 <= radius ** 2
+        m[mask] = 1
+        return m
