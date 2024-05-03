a3443b5f6cb85894937f6be96d133185c45a64cc
Spexophis <billinpro@gmail.com>
2024-05-03 13:38:25 +0200

    Update tool_improc.py

--- miao/tools/tool_improc.py
+++ miao/tools/tool_improc.py
@@ -1,14 +1,13 @@
 import numpy as np
+from numpy.fft import fft2, ifft2, fftshift
 import cv2
 from skimage import filters
+import matplotlib.pyplot as plt
 
 wl = 0.5  # wavelength in microns
 na = 1.4  # numerical aperture
-dx = 13 / (2 * 5 * 63 / 3)  # pixel size in microns
-nx = 1024  # size of region
+dx = 0.0785  # pixel size in microns
 fs = 1 / dx  # Spatial sampling frequency, inverse microns
-df = fs / nx  # Spacing between discrete frequency coordinates, inverse microns
-radius = (na / wl) / df
 
 
 def rms(data):
@@ -25,7 +24,7 @@ def img_properties(img):
 
 
 def fourier_transform(data):
-    return np.log(np.abs(np.fft.fftshift(np.fft.fft2(data))))
+    return np.log(np.abs(fftshift(fft2(data))))
 
 
 def disc_array(shape=(128, 128), radi=64.0, origin=None, dtp=np.float64):
@@ -52,12 +51,14 @@ def gaussian_filter(shape, sigma, pv, orig=None):
         uy = _ny / 2.
     else:
         ux, uy = orig
-    g = pv * np.fromfunction(lambda i, j: np.exp(-((i - ux) ** 2. + (j - uy) ** 2.) / (2. * sigma ** 2.)), (_nx, _ny))
-    return g
+    g = np.fromfunction(lambda i, j: np.exp(-((i - ux) ** 2. + (j - uy) ** 2.) / (2. * sigma ** 2.)), (_nx, _ny))
+    return pv * g
 
 
 def snr(img, lpr, hpr, gau=False):
     _ny, _nx = img.shape
+    df = fs / _nx
+    radius = (na / wl) / df
     m = img.min()
     img[img <= m] = 0.
     img[img > m] = img[img > m] - m
@@ -73,6 +74,8 @@ def snr(img, lpr, hpr, gau=False):
 
 def hpf(img, hpr, gau=True):
     _nx, _ny = img.shape
+    df = fs / _nx
+    radius = (na / wl) / df
     m = img.min()
     img[img <= m] = 0.
     img[img > m] = img[img > m] - m
@@ -139,3 +142,99 @@ def calculate_focus_measure_with_laplacian(image):
     laplacian_image = filters.laplace(image)
     focus_measure = np.var(laplacian_image)
     return focus_measure
+
+
+def autocorrelation_2d_fft(image):
+    # FFT of the image
+    F = fft2(image)
+    # Element-wise multiplication of FFT of the image with its complex conjugate
+    F_conj = np.conj(F)
+    auto_corr_freq = F * F_conj
+    # Inverse FFT to convert back to spatial domain
+    auto_corr = ifft2(auto_corr_freq)
+    # Return the magnitude of the autocorrelation, shifted to center
+    return np.abs(fftshift(auto_corr))
+
+
+def meshgrid(nx_, ny_):
+    x_ = np.arange(-nx_ / 2, nx_ / 2)
+    y_ = np.arange(-ny_ / 2, ny_ / 2)
+    xv_, yv_ = np.meshgrid(x_, y_, indexing='ij', sparse=True)
+    return np.roll(xv_, int(nx_ / 2)), np.roll(yv_, int(ny_ / 2))
+
+
+def shift(arr, shifts=None):
+    if shifts is None:
+        shifts = np.array(arr.shape) / 2
+    if len(arr.shape) == len(shifts):
+        for m, p in enumerate(shifts):
+            arr = np.roll(arr, int(p), m)
+    return arr
+
+
+def get_pupil(nx_, rd_):
+    msk = shift(disc_array(shape=(nx_, nx_), radi=rd_)) / np.sqrt(np.pi * rd_ ** 2) / nx_
+    phi = np.zeros((nx_, nx_))
+    return msk * np.exp(1j * phi)
+
+
+def get_psf(nx_, rd_):
+    bpp = get_pupil(nx_, rd_)
+    psf_ = np.abs((fft2(fftshift(bpp)))) ** 2
+    return psf_ / psf_.sum()
+
+
+def get_correlation(image_to_be_computed, shift_orientation, shift_spacing, xv, yv, psf):
+    kx = dx * np.cos(shift_orientation) / shift_spacing
+    ky = dx * np.sin(shift_orientation) / shift_spacing
+    ysh = np.exp(2j * np.pi * (kx * xv + ky * yv))
+    otf0 = fft2(psf)
+    imgf0 = fft2(image_to_be_computed)
+    otf = fft2(psf * ysh)
+    imgf = fft2(image_to_be_computed * ysh)
+    a = np.sum(imgf0 * np.conj(otf0) * np.conj(imgf * np.conj(otf)))
+    return np.abs(a), np.angle(a)
+
+
+def find_pattern(data, angle=0., spacing=0.268, nps=10, r_ang=0.005, r_sp=0.005, verbose=False):
+    nx, ny = data.shape
+    df = fs / nx
+    radius = (na / wl) / df
+    xv_, yv_ = meshgrid(nx, nx)
+    psf_ = get_psf(nx, radius)
+    d_ang = 2 * r_ang / nps
+    d_sp = 2 * r_sp / nps
+    ang_iter = np.arange(-r_ang, r_ang + d_ang / 2, d_ang) + angle
+    sp_iter = np.arange(-r_sp, r_sp + d_sp / 2, d_sp) + spacing
+    mags = np.zeros((nps + 1, nps + 1))
+    phs = np.zeros((nps + 1, nps + 1))
+    for m, ang in enumerate(ang_iter):
+        for n, sp in enumerate(sp_iter):
+            mag, phase = get_correlation(data, ang, sp, xv_, yv_, psf_)
+            if np.isnan(mag):
+                mags[m, n] = 0.0
+            else:
+                mags[m, n] = mag
+                phs[m, n] = phase
+    if verbose:
+        plt.figure()  # Set the figure size for better visualization
+        plt.subplot(211)  # First subplot for magnitudes
+        plt.imshow(mags, vmin=mags.min(), vmax=mags.max(),
+                   extent=(sp_iter.min(), sp_iter.max(), ang_iter.max(), ang_iter.min()),
+                   interpolation='none', cmap='viridis')
+        plt.colorbar()
+        plt.title('Magnitude')
+        plt.xlabel('Spatial Iteration')
+        plt.ylabel('Angular Iteration')
+        plt.subplot(212)
+        plt.imshow(phs, interpolation='none', cmap='twilight')
+        plt.colorbar()
+        plt.title('Phase')
+        plt.xlabel('X-axis')
+        plt.ylabel('Y-axis')
+        plt.tight_layout()
+        plt.show()
+    m, n = np.where(mags == mags.max())
+    ang_max = m[0] * d_ang - r_ang + angle
+    sp_max = n[0] * d_sp - r_sp + spacing
+    return ang_max, sp_max, mags[m, n]
