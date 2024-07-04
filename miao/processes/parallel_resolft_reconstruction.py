207240b26517afc4f33c0b2b3804c7442b18125e
Spexophis <billinpro@gmail.com>
2024-07-04 17:28:09 +0200

    Update parallel_resolft_reconstruction.py

--- miao/processes/parallel_resolft_reconstruction.py
+++ miao/processes/parallel_resolft_reconstruction.py
@@ -1,5 +1,7 @@
 import numpy as np
 import tifffile as tf
+from numpy.fft import fft2, fftshift
+from skimage.feature import peak_local_max
 
 
 class ImageReconstruction:
@@ -34,15 +36,37 @@ class ImageReconstruction:
         self.x_coords = np.arange(self.range_x[0], self.range_x[1] + 1, self.period_x)
         self.y_coords = np.arange(self.range_y[0], self.range_y[1] + 1, self.period_y)
 
-    def create_gaussian_array(self):
+    def create_gaussian_1d_array(self, x=True):
+        array = np.zeros((self.ny, self.nx))
+
+        # Place 1D Gaussians on the grid along the one direction
+        if x:
+            for x_center in self.x_coords:
+                kernel = self.gaussian_kernel_1d(self.kernel_size, self.sigma, x_center % 1)
+                for y in range(self.ny):
+                    x_start = max(0, int(x_center) - self.kernel_size // 2)
+                    x_end = min(self.nx, int(x_center) + self.kernel_size // 2 + 1)
+                    array[y, x_start:x_end] += kernel[(self.kernel_size // 2 - (int(x_center) - x_start)):(
+                                self.kernel_size // 2 + (x_end - int(x_center)))]
+        else:
+            for y_center in self.y_coords:
+                kernel = self.gaussian_kernel_1d(self.kernel_size, self.sigma, y_center % 1)
+                for x in range(self.nx):
+                    y_start = max(0, int(y_center) - self.kernel_size // 2)
+                    y_end = min(self.ny, int(y_center) + self.kernel_size // 2 + 1)
+                    array[y_start:y_end, x] += kernel[(self.kernel_size // 2 - (int(y_center) - y_start)):(
+                                self.kernel_size // 2 + (y_end - int(y_center)))]
+        return array
+
+    def create_gaussian_2d_array(self):
         array = np.zeros((self.ny, self.nx))
         
-        # Place Gaussians on the grid
+        # Place 2D Gaussians on the grid
         for x_center in self.x_coords:
             for y_center in self.y_coords:
                 # Calculate sub-pixel offset for Gaussian center
                 center = (x_center % 1, y_center % 1)
-                gaussian_k = self.gaussian_kernel(self.kernel_size, self.sigma, center)
+                gaussian_k = self.gaussian_kernel_2d(self.kernel_size, self.sigma, center)
                 x_idx = int(x_center)
                 y_idx = int(y_center)
                 # Place the Gaussian in the array, handling edges
@@ -82,7 +106,34 @@ class ImageReconstruction:
         center_positions = np.array(center_positions)
         return stacked_subarrays, center_positions
 
-    def gaussian_kernel(self, size, sigma, center):
+    def extract_periods(self):
+        image = np.average(self.data_stack, axis=0)
+        fft_image = fftshift(fft2(image))
+        magnitude_spectrum = np.abs(fft_image)
+        normalized_spectrum = np.log1p(magnitude_spectrum)
+        # Identify peaks in the magnitude spectrum
+        peaks = peak_local_max(normalized_spectrum, min_distance=10, threshold_rel=0.1)
+        # Compute the distances from the center of the Fourier image
+        center = np.array(normalized_spectrum.shape) // 2
+        peak_distances = np.sqrt((peaks[:, 0] - center[0]) ** 2 + (peaks[:, 1] - center[1]) ** 2)
+        # Sort distances and select the most prominent peaks
+        sorted_indices = np.argsort(peak_distances)
+        sorted_peaks = peaks[sorted_indices]
+        # Compute the periods corresponding to the prominent peaks
+        periods = []
+        for peak in sorted_peaks[1:5]:  # Consider the two most prominent peaks
+            distance = np.sqrt((peak[0] - center[0]) ** 2 + (peak[1] - center[1]) ** 2)
+            period = (image.shape[0] / distance) * self.pixel_size
+            periods.append(period)
+        return periods, normalized_spectrum, sorted_peaks[1:5]
+
+    def gaussian_kernel_1d(self, size, sigma, center):
+        x = np.linspace(-size // 2, size // 2, size) - center
+        kernel = np.exp(-(x ** 2) / (2 * sigma ** 2))
+        kernel[kernel < self.threshold] = 0
+        return kernel
+
+    def gaussian_kernel_2d(self, size, sigma, center):
         x = np.linspace(-size // 2, size // 2, size) - center[0]
         y = np.linspace(-size // 2, size // 2, size) - center[1]
         x, y = np.meshgrid(x, y)
