ea7f7293150f3305ecea250d3088e38c64a472a2
Spexophis <billinpro@gmail.com>
2024-07-04 13:56:20 +0200

    Update parallel_resolft_reconstruction.py

--- miao/processes/parallel_resolft_reconstruction.py
+++ miao/processes/parallel_resolft_reconstruction.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tifffile as tf
 
 
 class ImageReconstruction:
@@ -6,37 +7,36 @@ class ImageReconstruction:
     def __init__(self):
         self.na = 1.4
         self.wl = 0.5
-        self.n, self.nx, self.ny = 27 * 27, 1024, 1024
+        self.resolution = self.wl / (2 * self.na)
         self.pixel_size = 0.063
+        # Gaussian kernel
+        self.sigma = to_sigma(self.resolution, self.pixel_size)
+        self.threshold = 0.5
+        self.kernel_size = 6 * int(self.sigma) + 1
+        
+    def load_data(self, fd):
+        data_stack = tf.imread(fd)
+        self.n, self.ny, self.nx = data_stack.shape
         self.dfx, self.dfy = 1 / (self.nx * self.pixel_size), 1 / (self.ny * self.pixel_size)
         self.rbx, self.rby = (self.na / self.wl) / self.dfx, (self.na / self.wl) / self.dfy
-        # Parameters
-        self.fwhm = self.wl / (2 * self.na)
-        self.sigma = fwhm_to_sigma(self.fwhm, self.pixel_size)
-        self.period_x_um = 0.83  # um
-        self.period_y_um = 0.83  # um
-        self.size_x = 1024  # Size of the array in x direction (pixels)
-        self.size_y = 1024  # Size of the array in y direction (pixels)
-        self.range_x_um = (20.26, 43.504)  # Range in x direction in micrometers
-        self.range_y_um = (20.26, 43.504)  # Range in y direction in micrometers
-        self.threshold = 0.5  # Threshold value
-
+        
+    def set_parameters(self, periods=(0.83, 0.83), ranges=((20.26, 43.504), (20.26, 43.504))):
+        self.period_x_um, self.period_y_um = periods  # micrometer
+        self.range_x_um, self.range_y_um = ranges  # micrometers
+        
         # Convert periods and ranges to pixel coordinates
         self.period_x = int(self.period_x_um / self.pixel_size)
         self.period_y = int(self.period_y_um / self.pixel_size)
         self.range_x = (self.range_x_um[0] / self.pixel_size, self.range_x_um[1] / self.pixel_size)
         self.range_y = (self.range_y_um[0] / self.pixel_size, self.range_y_um[1] / self.pixel_size)
 
-        # Create the 2D array
-        self.array = np.zeros((self.size_y, self.size_x))
-
-        # Create grid of points, include endpoint
+        # Create grid of points
         self.x_coords = np.arange(self.range_x[0], self.range_x[1] + 1, self.period_x)
         self.y_coords = np.arange(self.range_y[0], self.range_y[1] + 1, self.period_y)
 
-        # Create a Gaussian kernel
-        self.kernel_size = 6 * int(self.sigma) + 1
-
+    def create_gaussian_array(self):
+        array = np.zeros((self.ny, self.nx))
+        
         # Place Gaussians on the grid
         for x_center in self.x_coords:
             for y_center in self.y_coords:
@@ -48,9 +48,9 @@ class ImageReconstruction:
                 # Place the Gaussian in the array, handling edges
                 x_start = max(0, x_idx - self.kernel_size // 2)
                 y_start = max(0, y_idx - self.kernel_size // 2)
-                x_end = min(self.size_x, x_idx + self.kernel_size // 2 + 1)
-                y_end = min(self.size_y, y_idx + self.kernel_size // 2 + 1)
-                self.array[y_start:y_end, x_start:x_end] += gaussian_k[
+                x_end = min(self.nx, x_idx + self.kernel_size // 2 + 1)
+                y_end = min(self.ny, y_idx + self.kernel_size // 2 + 1)
+                array[y_start:y_end, x_start:x_end] += gaussian_k[
                                                        (self.kernel_size // 2 - (y_idx - y_start)):(
                                                                self.kernel_size // 2 + (y_end - y_idx)),
                                                        (self.kernel_size // 2 - (x_idx - x_start)):(
@@ -58,8 +58,10 @@ class ImageReconstruction:
                                                        ]
 
         # Apply the threshold
-        self.array[self.array < self.threshold] = 0
+        array[array < self.threshold] = 0
+        return array
 
+    def stack_subarray(self, array):
         # Crop sub-arrays and stack into a 3D array
         stacked_subarrays = []
         center_positions = []
@@ -70,9 +72,9 @@ class ImageReconstruction:
                 y_idx = int(y_center)
                 x_start = max(0, x_idx - self.period_x // 2)
                 y_start = max(0, y_idx - self.period_y // 2)
-                x_end = min(self.size_x, x_start + self.period_x)
-                y_end = min(self.size_y, y_start + self.period_y)
-                subarray = self.array[y_start:y_end, x_start:x_end]
+                x_end = min(self.nx, x_start + self.period_x)
+                y_end = min(self.ny, y_start + self.period_y)
+                subarray = array[y_start:y_end, x_start:x_end]
 
                 # Ensure the subarray is of the desired size
                 if subarray.shape == (self.period_y, self.period_x):
@@ -81,6 +83,7 @@ class ImageReconstruction:
 
         stacked_subarrays = np.array(stacked_subarrays)
         center_positions = np.array(center_positions)
+        return stacked_subarrays, center_positions
 
 
 def gaussian_kernel(size, sigma, center):
@@ -92,8 +95,8 @@ def gaussian_kernel(size, sigma, center):
     return kernel
 
 
-def fwhm_to_sigma(fwhm, pixel_size):
-    return (fwhm / pixel_size) / (2 * np.sqrt(2 * np.log(2)))
+def to_sigma(fw, pixel_size):
+    return (fw / pixel_size) / (2 * np.sqrt(2 * np.log(2)))
 
 
 if __name__ == "__main__":
